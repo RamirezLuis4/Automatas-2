22881a5a938470b264276558a285ab77b6612292
Luis Angel Ramirez Peña <tprog.luis.ramirez@gmail.com>
2022-09-09 09:33:12 -0500

    first commit

---
+++ prueba.cpp
@@ -0,0 +1,14 @@
+#include <iostream.h>
+#include <stdio.h>
+#include <conio.h>
+float resultado;
+int a;
+char opcion;
+float radio, altura, x;
+// Este programa calcula el volumen de un cilindro.
+void main()
+{
+    x = 10;
+    printf("resultado = ");
+    printf((3 + 5)*8 - (10 - 4)/2);
+}
